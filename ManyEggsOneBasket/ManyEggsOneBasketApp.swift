6e29c1947ad81ab601a407e46cd91a42355c43df
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-02-05 16:54:02 -0600

    Initial Commit

---
+++ ManyEggsOneBasket/ManyEggsOneBasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManyEggsOneBasketApp.swift
+//  ManyEggsOneBasket
+//
+//  Created by Geoffrey Johnson on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ManyEggsOneBasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
